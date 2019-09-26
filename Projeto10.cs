e1b3dd5570b32506ec3b08fd6a173b098343ef3e
kaduramon10 <55710636+kaduramon10@users.noreply.github.com>
2019-09-26 16:31:14 -0300

    Update Projeto10.cs

--- Projeto10.cs
+++ Projeto10.cs
@@ -1,20 +1,21 @@
 using System;
 
-namespace projeto10
+
+namespace projeto10 //*nome do projeto  *//
 {
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            string repete;
+            string repete; //*criando a variavel repete*//
             do
             {
-                Console.Write("Entre com o número: ");
-                string textoTela = Console.ReadLine();
-                string extenso = string.Empty;
-                string textoAux = ("000000" + textoTela).Substring(textoTela.Length);
+                Console.Write("Entre com o número: "); //* Escreve  para entrar com o numero *//
+                string textoTela = Console.ReadLine();//*Armazena o numero na variavel textoTela*//
+                string extenso = string.Empty;//*cria a variavel com o valor vazio *//
+                string textoAux = ("000000" + textoTela).Substring(textoTela.Length);/*remove os numeros deixando apenas 6 casas ou completa com zeros */
 
-                extenso = ProcessarCentenaMilhar(textoAux);
+                extenso = ProcessarCentenaMilhar(textoAux);/*chama o metodo de processamento que retorno o num extenso armazenado na variavel  */
 
                 Console.WriteLine(extenso);
                 Console.WriteLine("digite \"S\" para repetir");
@@ -22,34 +23,75 @@ namespace projeto10
             } while (repete.ToUpper() == "S");
         }
 
-        private static string ProcessarCentenaMilhar(string texto)
+        private static string ProcessarCentenaMilhar(string texto) /*metodo criado para processar centenademilhar */
         {
             string extenso;
-            string centenaMilhar = texto.Substring(0, 3);
-            string centena = texto.Substring(3, 3);
-            if (centenaMilhar == "000")
+            string centenaMilhar = texto.Substring(0, 3);/*pega os 3 primeiros numeros das 6 posições*/
+            string centena = texto.Substring(3, 3);/*pega os 3 ultimos numeros das 6 posições*/
+            if (centenaMilhar == "000")/*Verifica se os 3 numeros da variavel centenaMilhar são 000*/
             {
-                extenso = ProcessarCentena(centena);
+                extenso = ProcessarCentena(centena);/*Metodo que processa o numero digitado  */
+            }
+            else if (centena == "000")
+            {
+                extenso = ProcessarCentena(centenaMilhar) + " mil"; //*Verifica se os 3 numeros da variavel centena são 000*//
             }
             else
             {
-                extenso = ProcessarCentena(centenaMilhar) + " mil e " + ProcessarCentena(centena);
+                extenso = ProcessarCentena(centenaMilhar) + " mil e " + ProcessarCentena(centena); //*Caso tenha numeros digitados na milhar e centena processa os 2 pq é uma milhar*//
             }
             return extenso;
         }
 
-        private static string ProcessarDezena(string texto)
+        private static string ProcessarCentena(string texto)/*criando o metodo processarCentena*/
         {
-            string extenso;
-            string dezena = texto.Substring(0, 1);
-            string unidade = texto.Substring(1, 1);
-            switch (dezena)
+            string extensoCentena;/*criando a variavel*/
+            string centena = texto.Substring(0, 1);/* separa  a centena em uma variavel*/
+            string dezena = texto.Substring(1, 2);/*separa  a dezena em uma variavel*/
+
+            switch (centena) /*verifica a centena para escrever seu extenso e processar a dezena final*/
             {
                 case "0":
-                    extenso = ProcessarUnidade(unidade);
+                    extensoCentena = ProcessarDezena(dezena);
                     break;
                 case "1":
-                    extenso = ProcessarDezena(texto);
+                    extensoCentena = centena == "00" ? "cem" : "cento e " + ProcessarDezena(dezena);
+                    break;
+                case "2":
+                    extensoCentena = dezena == "00" ? "duzentos" : "duzentos e " + ProcessarDezena(dezena); break;
+                case "3":
+                    extensoCentena = dezena == "00" ? "trezentos" : "trezentos e " + ProcessarDezena(dezena); break;
+                case "4":
+                    extensoCentena = dezena == "00" ? "quatrocentos" : "quatrocentos e " + ProcessarDezena(dezena); break;
+                case "5":
+                    extensoCentena = dezena == "00" ? "quinhentos" : "quinhentos e " + ProcessarDezena(dezena); break;
+                case "6":
+                    extensoCentena = dezena == "00" ? "seisentos" : "seisentos e " + ProcessarDezena(dezena); break;
+                case "7":
+                    extensoCentena = dezena == "00" ? "setecentos" : "setecentos e " + ProcessarDezena(dezena); break;
+                case "8":
+                    extensoCentena = dezena == "00" ? "oitocentos" : "oitocentos e " + ProcessarDezena(dezena); break;
+                case "9":
+                    extensoCentena = dezena == "00" ? "novecentos" : "novecentos e " + ProcessarDezena(dezena); break;
+                default:
+                    extensoCentena = ("não é número!");
+                    break;
+            }
+            return extensoCentena;
+        }
+
+        private static string ProcessarDezena(string texto)/*criando o metodo processarDezena*/
+        {
+            string extenso;
+            string dezena = texto.Substring(0, 1);/*separa qual a dezena do numero*/
+            string unidade = texto.Substring(1, 1);/*separa a unidade*/
+            switch (dezena)/*verifica qual a dezena para escrever seu extenso e processa a unidade final*/
+            {
+                case "0":
+                    extenso = ProcessarUnidade(unidade);/*se extenso for igual ProcessarUnidade ==processa unidade*/
+                    break;
+                case "1":
+                    extenso = ProcessarDezeNa(texto);/**se extenso for igual ProcessarDezena ==processa dezena*/
                     break;
                 case "2":
                     extenso = unidade == "0" ? "vinte" : "vinte e " + ProcessarUnidade(unidade);
@@ -82,52 +124,10 @@ namespace projeto10
             return extenso;
         }
 
-        private static string ProcessarDezeNa(string texto)
+        private static string ProcessarUnidade(string texto)/*criando o metodo processarUnidade*/
         {
-            string extensoDezena;
-            switch (texto)
-            {
-                case "10":
-                    extensoDezena = ("dez");
-                    break;
-                case "11":
-                    extensoDezena = ("onze");
-                    break;
-                case "12":
-                    extensoDezena = ("doze");
-                    break;
-                case "13":
-                    extensoDezena = ("treze");
-                    break;
-                case "14":
-                    extensoDezena = ("catorze");
-                    break;
-                case "15":
-                    extensoDezena = ("quize");
-                    break;
-                case "16":
-                    extensoDezena = ("dezeseis");
-                    break;
-                case "17":
-                    extensoDezena = ("dezesete");
-                    break;
-                case "18":
-                    extensoDezena = ("dezeoito");
-                    break;
-                case "19":
-                    extensoDezena = ("dezenove");
-                    break;
-                default:
-                    extensoDezena = ("não é número!");
-                    break;
-            }
-            return extensoDezena;
-        }
-
-        private static string ProcessarUnidade(string texto)
-        {
-            string extensoUnidade;
-            switch (texto)
+            string extensoUnidade;/*verifica qual a unidde para escrever seu extenso*/
+            switch (texto) /*/*verifica a unidade para escrever seu extenso e processar a unidade final*/
             {
                 case "0":
                     extensoUnidade = "zero";
@@ -165,45 +165,47 @@ namespace projeto10
             }
             return extensoUnidade;
         }
-        private static string ProcessarCentena(string texto)
 
-
+        private static string ProcessarDezeNa(string texto)
         {
-            string extensoCentena;
-            string centena = texto.Substring(0, 1);
-            string dezena = texto.Substring(1, 2);
-
-            switch (centena)
+            string extensoDezena;/*verifica qual a dexena para escrever seu extenso*/
+            switch (texto)/*/*verifica a dezena para escrever seu extenso e processar a dezena final*/
             {
-                case "0":
-                    extensoCentena = ProcessarDezena(dezena);
+                case "10":
+                    extensoDezena = ("dez");
                     break;
-                case "1":
-                    extensoCentena = centena == "00" ? "cem" : "cento e " + ProcessarDezena(dezena); break;
-                case "2":
-                    extensoCentena = dezena == "00" ? "duzentos" : "duzentos e " + ProcessarDezena(dezena); break;
-                case "3":
-                    extensoCentena = dezena == "00" ? "trezentos" : "trezentos e " + ProcessarDezena(dezena); break;
-                case "4":
-                    extensoCentena = dezena == "00" ? "quatrocentos" : "quatrocentos e " + ProcessarDezena(dezena); break;
-                case "5":
-                    extensoCentena = dezena == "00" ? "quinhentos" : "quinhentos e " + ProcessarDezena(dezena); break;
-                case "6":
-                    extensoCentena = dezena == "00" ? "seisentos" : "seisentos e " + ProcessarDezena(dezena); break;
-                case "7":
-                    extensoCentena = dezena == "00" ? "setecentos" : "setecentos e " + ProcessarDezena(dezena); break;
-                case "8":
-                    extensoCentena = dezena == "00" ? "oitocentos" : "oitocentos e " + ProcessarDezena(dezena); break;
-                case "9":
-                    extensoCentena = dezena == "00" ? "novecentos" : "novecentos e " + ProcessarDezena(dezena); break;
+                case "11":
+                    extensoDezena = ("onze");
+                    break;
+                case "12":
+                    extensoDezena = ("doze");
+                    break;
+                case "13":
+                    extensoDezena = ("treze");
+                    break;
+                case "14":
+                    extensoDezena = ("catorze");
+                    break;
+                case "15":
+                    extensoDezena = ("quize");
+                    break;
+                case "16":
+                    extensoDezena = ("dezeseis");
+                    break;
+                case "17":
+                    extensoDezena = ("dezesete");
+                    break;
+                case "18":
+                    extensoDezena = ("dezeoito");
+                    break;
+                case "19":
+                    extensoDezena = ("dezenove");
+                    break;
                 default:
-                    extensoCentena = ("não é número!");
+                    extensoDezena = ("não é número!");
                     break;
             }
-            return extensoCentena;
-
+            return extensoDezena;
         }
-
-
     }
 }
